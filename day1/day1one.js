b65efcf79404bff0502528e8d9a848e04a1c48a2
Bryce Kennedy <bryce@brycekennedy.net>
2021-12-01 16:23:24 -0700

    Initial commit

---
+++ day1/day1one.js
@@ -0,0 +1,5 @@
+export default function countIncreases(measurements) {
+    let currentValue = measurements[1]
+    let previousValue = measurements[0]
+    const increases = 0
+}
---
+++ day1/day1one.test.js
@@ -0,0 +1,5 @@
+import countIncreases from "./day1one";
+
+test('should return the number of increases', () => {
+    expect(countIncreases([0, 1, 2, 3])).toEqual(3)
+})
